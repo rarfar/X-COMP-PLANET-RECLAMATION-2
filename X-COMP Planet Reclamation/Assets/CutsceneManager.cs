8da6982e2d522de629d35477f0140478457ec1bb
Kelly <keviwakeup.69@gmail.com>
2025-04-27 00:03:05 -0400

    UI & Cutscene Fix
    Fixed UI anchor to fit 1080 screen

--- X-COMP Planet Reclamation/Assets/CutsceneManager.cs
+++ X-COMP Planet Reclamation/Assets/CutsceneManager.cs
@@ -6,10 +6,17 @@ using UnityEngine.SceneManagement;
 public class CutsceneManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
 
-        SceneManager.LoadScene("V2_Level_0", LoadSceneMode.Single);
+    public float changeTime;
+    public string sceneName;
+    
+    void Update()
+    {
+        changeTime -= Time.deltaTime;
+        if ( changeTime < 0 )
+        {
+            SceneManager.LoadScene(sceneName, LoadSceneMode.Single);
+        }    
 
     }
 }
